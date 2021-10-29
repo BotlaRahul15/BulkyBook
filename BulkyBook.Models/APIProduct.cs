d0c1ecd850644a082174f7d5ba57929b10669262
BotlaRahul15 <91177870+BotlaRahul15@users.noreply.github.com>
2021-10-29 11:36:39 +0530

    Add files via upload

---
+++ BulkyBook.Models/APIProduct.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class APIProduct
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductFeatures { get; set; }
+        public int ProductPrice { get; set; }
+        public bool Status { get; set; }
+    }
+    public class Source
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Articles
+    {
+        public Source Source { get; set; }
+        public string author { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+        public string urlToImage { get; set; }
+        public string publishedAt { get; set; }
+        public string content { get; set; }
+    }
+    public class Resp
+    {
+        public string SearchBy { get; set; }
+        public string SourceName { get; set; }
+        public string Status { get; set; }
+        public string totalResults { get; set; }
+        public List<Articles> Articles { get; set; }
+    }
+}
---
+++ BulkyBook.Models/DataTableAjaxPostModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class DataTableAjaxPostModel
+    {
+        // properties are not capital due to json mapping
+        public int draw { get; set; }
+        public int start { get; set; }
+        public int length { get; set; }
+        public List<Column> columns { get; set; }
+        public Search search { get; set; }
+        public List<Order> order { get; set; }
+    }
+
+    public class Column
+    {
+        public string data { get; set; }
+        public string name { get; set; }
+        public bool searchable { get; set; }
+        public bool orderable { get; set; }
+        public Search search { get; set; }
+    }
+
+    public class Search
+    {
+        public string value { get; set; }
+        public string regex { get; set; }
+    }
+
+    public class Order
+    {
+        public int column { get; set; }
+        public string dir { get; set; }
+    }
+    public class JqueryDatatableParam
+    {
+        public string sEcho { get; set; }
+        public string sSearch { get; set; }
+        public int iDisplayLength { get; set; }
+        public int iDisplayStart { get; set; }
+        public int iColumns { get; set; }
+        public int iSortCol_0 { get; set; }
+        public string sSortDir_0 { get; set; }
+        public int iSortingCols { get; set; }
+        public string sColumns { get; set; }
+    }
+}
